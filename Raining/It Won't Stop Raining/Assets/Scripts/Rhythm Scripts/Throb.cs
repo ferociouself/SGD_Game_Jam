dc9a5cd757f0d8c577f1fdf53537e787917d8b25
zsd4yr <zsd4yr@virginia.edu>
2016-10-22 13:13:42 -0400

    Lerping, throbbing circle bois
    yas

--- Raining/It Won't Stop Raining/Assets/Scripts/Rhythm Scripts/Throb.cs
+++ Raining/It Won't Stop Raining/Assets/Scripts/Rhythm Scripts/Throb.cs
@@ -4,23 +4,51 @@ using System.Collections;
 public class Throb : MonoBehaviour {
 
 	public float tempo;
-	public float timeSinceLast;
+	public float scale;
+	public float LERP_TIME = 0.5f;
 
-	// Use this for initialization
+	float timeSinceLast;
+	Vector3 initPos;
+	Vector3 finalPos;
+	float transTime = 0.0f;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
 	void Start () {
 		timeSinceLast = Time.time;
+		finalPos = gameObject.transform.localScale;
+		initPos = new Vector3(scale, scale, 1.0f);
+		Debug.Log ("Tempo: " + (tempo / 60).ToString());
 	}
 	
-	// Update is called once per frame
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
 	void Update () {
 		timeSinceLast += Time.deltaTime;
+		//Debug.Log ("TSL: " + timeSinceLast.ToString());
 		if (timeSinceLast >= tempo / 60) {
-			ThrobMe ();
-			timeSinceLast = 0;
+			if (RavageMe ()) {
+				timeSinceLast = 0;
+			}
 		}
 	}
 
-	private void ThrobMe(){
-		//Mathf.Lerp(
+	private bool RavageMe() {
+
+		float timerVal = transTime / LERP_TIME;
+
+	    this.gameObject.transform.localScale = Vector3.Lerp(this.initPos, this.finalPos, timerVal);
+
+		if (transTime >= LERP_TIME) {
+			gameObject.transform.localScale = finalPos;
+			transTime = 0.0f;
+			Debug.Log ("Finished");
+			return true;
+		}  else {
+			transTime += Time.deltaTime;
+			return false;
+		}
 	}
 }

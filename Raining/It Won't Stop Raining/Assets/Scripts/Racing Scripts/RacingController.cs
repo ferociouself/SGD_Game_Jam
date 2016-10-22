fcb7f6861df71b7d8239f2fdfc71bdbd72424c09
ferociouself <jhe3cd@virginia.edu>
2016-10-22 19:19:05 -0400

    Like I give a fuck
    nahp

---
+++ Raining/It Won't Stop Raining/Assets/Scripts/Racing Scripts/RacingController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RacingController : MonoBehaviour {
+
+	public float speed;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//memes
+		float curRot = gameObject.transform.rotation.eulerAngles.z;
+
+		Vector2 force = new Vector2(Mathf.Sin(Mathf.Deg2Rad * curRot), -Mathf.Cos(Mathf.Deg2Rad * curRot));
+
+		if (Input.GetButton("Up")) {
+			rb.AddForce(force * -speed);
+		}
+		if (Input.GetButton("Down")) {
+			rb.AddForce(force * speed);
+		}
+
+		if (Input.GetButton("Right")) {
+			rb.angularVelocity = -speed * 20;
+		}
+		else if (Input.GetButton("Left")) {
+			rb.angularVelocity = speed * 20;
+		}
+	}
+}

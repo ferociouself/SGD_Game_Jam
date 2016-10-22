0c6224ec65c8917ba4efc577b75ea92cdd865516
ferociouself <jhe3cd@virginia.edu>
2016-10-21 21:55:58 -0400

    Added some player movement, working on interactables.
    Woah dude

---
+++ Raining/It Won't Stop Raining/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 1.0f;
+	public float jumpForce = 1.0f;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horiz = Input.GetAxis("Horizontal") * speed;
+
+		rb.velocity = new Vector2(horiz, rb.velocity.y);
+
+		if (Input.GetButtonDown("Jump")) {
+			rb.AddForce(Vector2.up * jumpForce);
+		}
+	}
+}

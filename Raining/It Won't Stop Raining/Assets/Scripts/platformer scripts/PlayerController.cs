a282242e56b3e7d57906fdcad8d9b6929e0eb8e5
Soupdoop <jherd23@gmail.com>
2016-10-21 23:49:26 -0400

    Lots of Changes
    LOC

---
+++ Raining/It Won't Stop Raining/Assets/Scripts/platformer scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Vertical") > 0 && rb.velocity.y == 0) {
+			rb.velocity = new Vector2(rb.velocity.x,6);
+		}
+		rb.velocity = new Vector2(Input.GetAxis("Horizontal")*5,rb.velocity.y);
+	}
+}

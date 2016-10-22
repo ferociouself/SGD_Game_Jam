c6ac1c1e49330105a51c7a5be4bbf2052856de0c
ferociouself <jhe3cd@virginia.edu>
2016-10-21 21:54:04 -0400

    Added Static Methods Class
    Static Methods includes:
    Distance between Vector2 and Vector3
    Almost Equals between floats, Vector2, and Vector3 with float
    thresholds for each.

---
+++ Raining/It Won't Stop Raining/Assets/Scripts/StaticMethods.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class StaticMethods {
+
+	public static float Distance(Vector3 p1, Vector3 p2) {
+		return (p2 - p1).magnitude;
+	}
+
+	public static float Distance(Vector2 p1, Vector2 p2) {
+		return (p2 - p1).magnitude;
+	}
+
+	public static bool AlmostEquals(float f1, float f2, float threshold) {
+		return Mathf.Abs(f1 - f2) < threshold;
+	}
+
+	public static bool AlmostEquals(Vector2 p1, Vector2 p2, float threshold) {
+		return Distance(p1, p2) < threshold;
+	}
+
+	public static bool AlmostEquals(Vector3 p1, Vector3 p2, float threshold) {
+		return Distance(p1, p2) < threshold;
+	}
+}

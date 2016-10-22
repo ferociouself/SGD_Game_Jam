1af1a259582a5398f1165f0c1682f6611bd4ca53
zsd4yr <zsd4yr@virginia.edu>
2016-10-22 01:26:30 -0400

    checkin

---
+++ Raining/It Won't Stop Raining/Assets/Scripts/RhythmInputMgmt.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RhythmInputMgmt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

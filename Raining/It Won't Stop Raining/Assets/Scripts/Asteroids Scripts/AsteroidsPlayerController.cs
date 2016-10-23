b572bb8539601c88fa47946b5b30074269bf4c95
ferociouself <jhe3cd@virginia.edu>
2016-10-23 10:36:02 -0400

    Beginnings of Asteroids

---
+++ Raining/It Won't Stop Raining/Assets/Scripts/Asteroids Scripts/AsteroidsPlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidsPlayerController : MonoBehaviour {
+
+	public float speed;
+
+	Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float curRot = gameObject.transform.rotation.eulerAngles.z;
+
+		Vector2 force = new Vector2(Mathf.Sin(Mathf.Deg2Rad * curRot), -Mathf.Cos(Mathf.Deg2Rad * curRot));
+
+		if (Input.GetButton("Up")) {
+			rb.AddForce(force * -speed);
+		}
+		if (Input.GetButton("Down")) {
+			rb.AddForce(force * speed);
+		}
+
+		if (Input.GetButton("Right")) {
+			rb.angularVelocity = -speed * 20;
+		}
+		else if (Input.GetButton("Left")) {
+			rb.angularVelocity = speed * 20;
+		}
+
+
+	}
+}

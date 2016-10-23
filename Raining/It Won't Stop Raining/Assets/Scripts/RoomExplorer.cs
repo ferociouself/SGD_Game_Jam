35c135d9d6ca972506973f198c4489dd53ba71bf
zsd4yr <zsd4yr@virginia.edu>
2016-10-23 17:04:17 -0400

    Wow that's a house
    mi casa es tu casa

---
+++ Raining/It Won't Stop Raining/Assets/Scripts/RoomExplorer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomExplorer : MonoBehaviour {
+
+	public GameObject parents;
+	public GameObject hall;
+	public GameObject kids;
+	public GameObject bath;
+	public GameObject living;
+	public GameObject kitchen;
+	public GameObject laundry;
+	public GameObject attic;
+	public GameObject basement;
+
+	private Color dark;
+	private Color dim;
+	private Color lit;
+
+	// Use this for initialization
+	void Start () {
+		dark = new Color(0x49,0x49,0x49,0xFF);
+		dim = new Color(0xB0,0xB0,0xB0,0xFF);
+		lit = new Color (0xFF,0xFF,0xFF,0xFF);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

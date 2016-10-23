3004b8ac406095e89c4bf81a1ad0777355246ec7
Clayton Smith <cps5aa@virginia.edu>
2016-10-22 22:45:39 -0400

    Hide and Seek
    wow Clayton is so good. Zach is super impressed.

---
+++ Raining/It Won't Stop Raining/Assets/Scripts/HideAndSeekScripts/PlayerMovementHAS.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerMovementHAS : MonoBehaviour
+{
+	public float speed;
+	private Rigidbody2D rb2d;
+	private bool hiding = false;
+	bool justHidden = false;
+	bool colliding = false;
+	public Text timer;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		timer.text = "Timer: ";
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		colliding = true;
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		colliding = false;
+	}
+
+	void Hide ()
+	{
+		justHidden = true;
+		gameObject.GetComponent<SpriteRenderer> ().enabled = !gameObject.GetComponent<SpriteRenderer> ().enabled;
+		hiding = !hiding;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float timeLeft = 15 - Time.deltaTime;
+		timer.text = "Timer: " + timeLeft.ToString ();
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		if (!hiding && colliding) {
+			if (Input.GetButtonDown ("Activate")) {
+				Hide ();
+			}
+		}
+		if (!hiding)
+			rb2d.velocity = new Vector2 (moveHorizontal * speed, moveVertical * speed);
+		else if (!justHidden) {
+			if (Input.GetButtonDown ("Activate")) {
+				Hide ();
+			}
+		}
+		if (justHidden) {
+			justHidden = false;
+		}
+
+	}
+}
--- Raining/It Won't Stop Raining/Assets/Scripts/House Scripts/PlayerMovement.cs
+++ Raining/It Won't Stop Raining/Assets/Scripts/House Scripts/PlayerMovement.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
 	public float speed = 1.0f;
 	public float jumpForce = 1.0f;
@@ -9,18 +10,20 @@ public class PlayerMovement : MonoBehaviour {
 	Rigidbody2D rb;
 
 	// Use this for initialization
-	void Start () {
-		rb = gameObject.GetComponent<Rigidbody2D>();
+	void Start ()
+	{
+		rb = gameObject.GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		float horiz = Input.GetAxis("Horizontal") * speed;
+	void Update ()
+	{
+		float horiz = Input.GetAxis ("Horizontal") * speed;
 
-		rb.velocity = new Vector2(horiz, rb.velocity.y);
+		rb.velocity = new Vector2 (horiz, rb.velocity.y);
 
-		if (Input.GetButtonDown("Jump")) {
-			rb.AddForce(Vector2.up * jumpForce);
+		if (Input.GetButtonDown ("Jump")) {
+			rb.AddForce (Vector2.up * jumpForce);
 		}
 	}
 }

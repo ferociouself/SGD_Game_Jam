33d6883c97ecfad1eea3b98d207ab1f74cd20d36
Clayton Smith <cps5aa@virginia.edu>
2016-10-23 14:54:36 -0400

    Newest
    I got the timer to work

--- Raining/It Won't Stop Raining/Assets/Scripts/HideAndSeekScripts/PlayerMovementHAS.cs
+++ Raining/It Won't Stop Raining/Assets/Scripts/HideAndSeekScripts/PlayerMovementHAS.cs
@@ -10,12 +10,22 @@ public class PlayerMovementHAS : MonoBehaviour
 	bool justHidden = false;
 	bool colliding = false;
 	public Text timer;
+	float timeLeft;
+	bool firstTimeComplete;
+	bool secondTimeComplete;
+	bool canControl;
+	float moveHorizontal;
+	float moveVertical;
 
 	// Use this for initialization
 	void Start ()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
 		timer.text = "Timer: ";
+		timeLeft = 15;
+		firstTimeComplete = false;
+		secondTimeComplete = false;
+		canControl = true;
 	}
 
 	void OnTriggerEnter2D (Collider2D other)
@@ -38,15 +48,51 @@ public class PlayerMovementHAS : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		float timeLeft = 15 - Time.deltaTime;
-		timer.text = "Timer: " + timeLeft.ToString ();
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		//Make it so that the timer counts down. Once when the inaugural timer ends, the second timer starts. The player object
+		//may not move. If the player is not hiding then the dog immediately goes to the player. Once when the Second timer
+		//hits 0, the dog leaves and the boy's sprite rendered is rendered again.
+
+		//Makes timer decrement. For all timers
+		timeLeft -= Time.deltaTime; 
+
+		//If the inaugural timer hits 0. reset timer to 30 and make first time to ture and disable controls
+		if (timeLeft <= 0 && firstTimeComplete == false) {
+			timeLeft = 30f;
+			firstTimeComplete = true;
+			canControl = false;
+		}
+		if (firstTimeComplete == true) {
+			rb2d.velocity = Vector3.zero;
+			rb2d.Sleep ();
+		}
+		//If the second timer hits 0, disable the timer
+		if (timeLeft <= 0 && firstTimeComplete == true && secondTimeComplete == false) {
+			timer.enabled = false;
+		}
+
+		//Do the ceiliing of the timer
+		timer.text = "Timer: " + (int)Mathf.Ceil (timeLeft);
+		//If the player can be controlled i.e. the first timer hasn't ended, move the player
+		if (canControl) {
+			moveHorizontal = Input.GetAxis ("Horizontal");
+			moveVertical = Input.GetAxis ("Vertical");
+		}
+		//If the player is not hiding and is touching a hidable object then hit 'e' and then hide the object and make the 
+		//Velocity 0
 		if (!hiding && colliding) {
 			if (Input.GetButtonDown ("Activate")) {
 				Hide ();
+				//Print if the object is hiding
+				print ("Hiding: " + hiding);
+				print ("Just Hidden: " + justHidden);
+
+				rb2d.velocity = Vector3.zero;
+				rb2d.Sleep ();
 			}
 		}
+
+		//If the Player is not 0 then make the velocity = to the movement * speed.
+		//Else if hiding then hit 'e' to unhide the object
 		if (!hiding)
 			rb2d.velocity = new Vector2 (moveHorizontal * speed, moveVertical * speed);
 		else if (!justHidden) {

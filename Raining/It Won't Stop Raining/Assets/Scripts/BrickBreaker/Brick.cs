95b19d3d99fda120f146eb2ff229bff042fec289
Soupdoop <jherd23@gmail.com>
2016-10-23 12:01:49 -0400

    Bump

---
+++ Raining/It Won't Stop Raining/Assets/Scripts/BrickBreaker/Brick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+	public int pointValue;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "ball") {
+			sendPoints ();
+			Destroy (this.gameObject);
+		}
+	}
+
+	void sendPoints(){
+		//Score Points?
+	}
+}

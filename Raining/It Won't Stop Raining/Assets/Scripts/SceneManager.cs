17c40e3f3a9d84ce302181e2a9f9b19c3c7fad44
zsd4yr <zsd4yr@virginia.edu>
2016-10-23 09:50:33 -0400

    Scene numbers

--- Raining/It Won't Stop Raining/Assets/Scripts/SceneManager.cs
+++ Raining/It Won't Stop Raining/Assets/Scripts/SceneManager.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class SceneManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public static SceneManager s_instance;
+
+	/// <summary>
+	/// Awaken this instance.
+	/// </summary>
+	void Awake()
+	{
+		if (s_instance == null)
+		{
+			DontDestroyOnLoad(gameObject); // save object on scene mvm
+			s_instance = this;
+		}
+		else if (s_instance != this)
+		{
+			Destroy(gameObject);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update () { }
+
+
+	/// <summary>
+	/// Moves to scene. Go the the Project Manager to ensure this.
+	/// </summary>
+	/// <returns><c>true</c>, if to scene was moved, <c>false</c> otherwise.</returns>
+	/// <param name="sceneNumber">Scene number.</param>
+	public void MoveToScene(int sceneNumber) {
+		Application.LoadLevel (sceneNumber);
 	}
 }

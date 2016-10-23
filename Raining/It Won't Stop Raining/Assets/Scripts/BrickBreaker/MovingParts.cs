e92c619d9467276baa01cf222e2fc5708fd7dc95
ferociouself <jhe3cd@virginia.edu>
2016-10-23 16:29:22 -0400

    Did this work?
    Probably not

---
+++ Raining/It Won't Stop Raining/Assets/Scripts/BrickBreaker/MovingParts.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingParts : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = new Vector2 (speed * Input.GetAxis ("Horizontal"), 0);
+	}
+}

8bd3241eca7a9c7f33af43368418a047fbcde50f
zsd4yr <zsd4yr@virginia.edu>
2016-10-23 02:25:21 -0400

    more notes wow

--- Raining/It Won't Stop Raining/Assets/Scripts/Rhythm Scripts/RhythmController.cs
+++ Raining/It Won't Stop Raining/Assets/Scripts/Rhythm Scripts/RhythmController.cs
@@ -54,8 +54,174 @@ public class RhythmController : MonoBehaviour {
 		mArr.Add(new Node (GenBirthTime(17.100f), SPEED_V, Vector2.down));
 		mArr.Add(new Node (GenBirthTime(17.400f), SPEED_H, Vector2.left));
 
+		mArr.Add(new Node (GenBirthTime(17.987f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(18.358f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(18.823f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(19.194f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(19.600f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(21.512f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(22.000f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(22.577f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(23.164f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(23.776f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(24.920f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(25.363f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(25.628f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(26.106f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(26.477f), SPEED_V, Vector2.up));
+
+		mArr.Add(new Node (GenBirthTime(27.957f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(28.328f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(28.607f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(30.576f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(30.970f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(31.268f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(31.640f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(32.104f), SPEED_H, Vector2.right));
+
+		mArr.Add(new Node (GenBirthTime(34.994f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(35.290f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(35.644f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(37.223f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(37.781f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(38.470f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(38.850f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(39.100f), SPEED_H, Vector2.right));
+
+		mArr.Add(new Node (GenBirthTime(41.826f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(42.383f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(42.550f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(44.155f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(44.712f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(45.340f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(45.733f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(46.100f), SPEED_H, Vector2.right));
+
+		mArr.Add(new Node (GenBirthTime(48.809f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(49.200f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(49.552f), SPEED_H, Vector2.right));	
+		mArr.Add(new Node (GenBirthTime(54.925f), SPEED_H, Vector2.right)); //long notes
+		mArr.Add(new Node (GenBirthTime(55.204f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(55.854f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(56.226f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(56.500f), SPEED_H, Vector2.right));
 
 
+		mArr.Add(new Node (GenBirthTime(58.113f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(58.400f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(58.763f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(58.900f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(59.100f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(59.310f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(59.650f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(60.000f), SPEED_H, Vector2.right));
+
+		mArr.Add(new Node (GenBirthTime(61.850f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(62.686f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(65.600f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(66.230f), SPEED_H, Vector2.left));
+
+		mArr.Add(new Node (GenBirthTime(66.694f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(67.159f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(67.344f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(67.716f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(68.087f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(68.400f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(68.799f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(69.109f), SPEED_V, Vector2.up));
+
+		mArr.Add(new Node (GenBirthTime(69.730f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(72.035f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(72.140f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(72.263f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(72.981f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(73.100f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(73.217f), SPEED_H, Vector2.left));
+
+		mArr.Add(new Node (GenBirthTime(74.590f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(74.693f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(74.796f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(74.900f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(75.050f), SPEED_H, Vector2.right));
+
+		mArr.Add(new Node (GenBirthTime(76.602f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(76.725f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(76.849f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(76.973f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(76.143f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(76.284f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(77.400f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(77.470f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(77.563f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(77.888f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(78.213f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(78.400f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(78.583f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(78.766f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(78.950f), SPEED_V, Vector2.down));
+
+		mArr.Add(new Node (GenBirthTime(79.100f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(79.211f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(79.323f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(80.098f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(80.250f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(80.411f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(80.573f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(80.734f), SPEED_H, Vector2.right));
+
+		mArr.Add(new Node (GenBirthTime(81.605f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(81.744f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(81.884f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(81.884f), SPEED_V, Vector2.down));
+
+		mArr.Add(new Node (GenBirthTime(83.269f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(83.269f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(83.400f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(83.400f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(83.530f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(83.530f), SPEED_H, Vector2.left));//climax, rock-maninov!
+
+		mArr.Add(new Node (GenBirthTime(86.316f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(86.706f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(87.174f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(87.400f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(89.369f), SPEED_V, Vector2.down));
+
+		mArr.Add(new Node (GenBirthTime(91.214f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(92.974f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(93.200f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(93.624f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(94.000f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(95.553f), SPEED_V, Vector2.down));
+
+		mArr.Add(new Node (GenBirthTime(97.590f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(97.590f), SPEED_H, Vector2.left));
+
+		mArr.Add(new Node (GenBirthTime(99.865f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(100.102f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(100.357f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(100.543f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(100.822f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(100.938f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(101.403f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(101.700f), SPEED_H, Vector2.right));
+
+		mArr.Add(new Node (GenBirthTime(104.650f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(104.932f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(105.120f), SPEED_H, Vector2.right));
+
+		mArr.Add(new Node (GenBirthTime(106.852f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(107.038f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(107.316f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(107.502f), SPEED_H, Vector2.right));
+		mArr.Add(new Node (GenBirthTime(107.761f), SPEED_V, Vector2.up));
+		mArr.Add(new Node (GenBirthTime(108.059f), SPEED_H, Vector2.left));
+		mArr.Add(new Node (GenBirthTime(108.270f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(108.368f), SPEED_V, Vector2.down));
+		mArr.Add(new Node (GenBirthTime(108.646f), SPEED_H, Vector2.right));
+
+
+		mArr.Add(new Node (GenBirthTime(2000.477f), SPEED_V, Vector2.up));
 		//dummy node at the end
 
 		mIndex = 0;

261268c92e8d0779a153bb39c954324c1af5cb1b
jpat64 <jrp3tu@virginia.edu>
2016-10-23 17:52:31 -0400

    EnemyHealth and getting rid of togglereactor

---
+++ Raining/It Won't Stop Raining/Assets/Scripts/TD/EnemyHealth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+    float health;
+	// Use this for initialization
+	void Start () {
+        health = 100;
+	}
+    
+    void takeDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    void checkDead()
+    {
+        if (health < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        checkDead();
+	}
+}
